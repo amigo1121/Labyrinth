e28b82adfcb16eaf7ae455d4381f7d7a5a2f59b5
amigo1121 <66857061+amigo1121@users.noreply.github.com>
2021-11-21 12:33:48 +0100

    drop

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 import { room } from "./objects.js";
 const maze = document.querySelector('.maze');
-const leftP = document.querySelector('#left');
+const remainDiv = document.querySelector('#left');
 const positionOfRooms = [];
 const fixedPositions = [];
 const dynamicPosition = [];
-let ID =0;
-let graph ;
-function initGraph(){
+let dragged;
+let ID = 0;
+let graph;
+function initGraph() {
    graph = new Array(7);
    for (let i = 0; i < graph.length; i++) {
-      graph[i]=new Array(7);
+      graph[i] = new Array(7);
    }
 }
 initGraph()
-let leftPiece ;
+let remainPiece;
 // console.log(positionOfRooms)
 
-function genImg(r,x,y)
-{
+function genImg(r, x, y) {
    const img = document.createElement('img');
-   img.src=`./img/${r.type}.png`
+   img.src = `./img/${r.type}.png`
    img.id = r.id;
    img.classList.add('room');
-   img.style.transform+=`rotate(${r.ang}deg)`;
-   img.style.top=`${x}px`;
-   img.style.left=`${y}px`;
+   img.style.transform += `rotate(${r.ang}deg)`;
+   img.style.top = `${x}px`;
+   img.style.left = `${y}px`;
    return img;
 }
 const fixedRooms =
@@ -72,61 +72,119 @@ function initMaze() {
    fixedRooms.forEach((e, i) => {
       const xPos = fixedPositions[i].x;
       const yPos = fixedPositions[i].y;
-      const r = new room( e[0], e[1],genId());
-      maze.append(genImg(r,xPos,yPos))
+      const r = new room(e[0], e[1], genId());
+      maze.append(genImg(r, xPos, yPos))
       console.log(r)
-      const x = fixedPositions[i].x/100;
-      const y = fixedPositions[i].y/100;
-      graph[x][y]=r;
+      const x = fixedPositions[i].x / 100;
+      const y = fixedPositions[i].y / 100;
+      graph[x][y] = r;
    })
-   const rid =[];
-   for(let i =0;i<13;i++)
-   {
+   const rid = [];
+   for (let i = 0; i < 13; i++) {
       rid.push('straight');
    }
-   for(let i =0;i<15;i++)
-   {
+   for (let i = 0; i < 15; i++) {
       rid.push('bend');
    }
-   for(let i =0;i<6;i++)
-   {
+   for (let i = 0; i < 6; i++) {
       rid.push('t-shape');
    }
-   rid.sort((a,b)=>(Math.random()*4-2));
+   rid.sort((a, b) => (Math.random() * 4 - 2));
    console.log(rid)
-   leftPiece = new room(rid.pop(),0,50)
-   leftP.append(genImg(leftPiece,0,0));
+   remainPiece = new room(rid.pop(), 0, 50)
+   remainDiv.append(genImg(remainPiece, 0, 0));
    console.log(rid)
    console.log(dynamicPosition)
-   rid.forEach((e,i)=>{
+   rid.forEach((e, i) => {
       const xPos = dynamicPosition[i].x;
-      const yPos =dynamicPosition[i].y;
-      const r = new room(e,Math.random()*4,genId());
-      maze.append(genImg(r,xPos,yPos))
-      const x= xPos/100;
-      const y= yPos/100;
-      graph[x][y]=r;
+      const yPos = dynamicPosition[i].y;
+      const r = new room(e, Math.random() * 4, genId());
+      maze.append(genImg(r, xPos, yPos))
+      const x = xPos / 100;
+      const y = yPos / 100;
+      graph[x][y] = r;
    })
 
 }
 initMaze();
 console.log(graph)
-function genId()
-{
+function genId() {
    ID++;
    return ID;
 }
 
-leftP.addEventListener('click',rotateIMG)
-function rotateIMG(event)
-{
+remainDiv.addEventListener('click', rotateIMG)
+function rotateIMG(event) {
    const t = event.target;
-   if(!t.matches('img'))
-   return;
-   t.style.transform+='rotate(90deg)';
-   leftPiece.beRotated();
-   // console.log(leftPiece.door);
+   if (!t.matches('img'))
+      return;
+   t.style.transform += 'rotate(90deg)';
+   remainPiece.beRotated();
+   // console.log(remainPiece.door);
 }
 // get ids from an row
 
+// TODO make extend piece draggable
+function makeRemainPieceDraggable() {
+   const obj = remainDiv.firstElementChild;
+   // console.log(obj)
+   if (obj) {
+      obj.setAttribute('draggable', 'true');
+   }
+}
+// makeRemainPieceDraggable()
 
+// TODO
+document.addEventListener("dragstart", function (event) {
+   // store a ref. on the dragged elem
+   dragged = event.target;
+   // make it half transparent
+   console.log(event.target)
+   event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function (event) {
+   // reset the transparency
+   event.target.style.opacity = "";
+}, false);
+
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function (event) {
+   // prevent default to allow drop
+   const t = event.target;
+   if (!t.matches('.dropable'))
+      return;
+   event.preventDefault();
+   // console.log(t)
+}, false);
+
+document.addEventListener("dragenter", function (event) {
+   // highlight potential drop target when the draggable element enters it
+   if (event.target.className.includes('dropable')) {
+      event.target.style.border = "2px solid yellow";
+      event.target.style.boxShadow = "0px 0px 10px yellow";
+  }
+
+}, false);
+
+document.addEventListener("dragleave", function (event) {
+   // reset background of potential drop target when the draggable element leaves it
+   if (event.target.className.includes('dropable')) {
+      event.target.style.border = "";
+      event.target.style.boxShadow = "";
+   }
+
+}, false);
+
+document.addEventListener("drop", function (event) {
+   // prevent default action (open as link for some elements)
+   event.preventDefault();
+   // move dragged elem to the selected drop target
+   if (event.target.className.includes('dropable'))  {
+      // event.target.style.background = "";
+      // dragged.parentNode.removeChild(dragged);
+      // event.target.appendChild(dragged);
+      console.log(object)
+   }
+}, false);
